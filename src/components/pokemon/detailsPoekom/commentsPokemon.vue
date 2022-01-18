0d135fdd1fb7372e76c858ae3f65ef588aa80cb7
Bryan Tacuri <bryantacuriar@gmail.com>
2022-01-17 21:03:47 -0500

    Avance Comments

---
+++ src/components/pokemon/detailsPoekom/commentsPokemon.vue
@@ -0,0 +1,62 @@
+<template>
+   <div class="containerrr">
+        <div class="comments">
+            <h4>Comentarios</h4>
+            <p>hola mi id es {{idPokemon}}</p>
+
+        </div>
+        <div v-if="commentExist">
+            
+        </div>
+        <div v-else>
+            <p>No existen comentarios</p>
+        </div>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Ingresa tu comentario" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="savedComments()">Bu</button>
+        </div>
+   </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            pokemonComment:[],
+            commentExist: Boolean,
+        }
+        
+    },
+    created() {
+        this.verificarArray()
+        
+    },
+    props:{
+        idPokemon:{
+            type: String,
+            required: true
+        },
+    },
+    methods:{
+        verificarArray(){
+            if (localStorage.getItem(this.idPokemon) === null) {
+                this.commentExist = false;
+
+            }else{
+                this.commentExist = true;
+
+            }
+             
+        },
+        getComments(){
+            const comentariosPokemon = localStorage.getItem(this.idPokemon);
+            this.pokemonComment = JSON.parse(comentariosPokemon);
+            console.log(comentariosPokemon);
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/pokemon/detailsPoekom/showDetailsPokemon.vue
+++ src/components/pokemon/detailsPoekom/showDetailsPokemon.vue
@@ -1,22 +1,40 @@
 <template>
     <Modal>
-        <div>
-            <img :src="pokemonInformation.sprites.front_default" alt="">
-            <img :src="pokemonInformation.sprites.back_default" alt="">
-            <h4>{{pokemonInformation.name}}</h4>
-            <p>Id = {{pokemonInformation.id}}</p>
-            <p>Peso = {{pokemonInformation.weight}}</p>
-            <p>Altura = {{pokemonInformation.height}}0 cm</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <img :src="pokemonInformation.sprites.front_default" alt="">
+                    <img :src="pokemonInformation.sprites.back_default" alt="">
+                    <h4>{{pokemonInformation.name}}</h4>
+                    <p>Id = {{pokemonInformation.id}}</p>
+                    <p>Peso = {{pokemonInformation.weight}}</p>
+                    <p>Altura = {{pokemonInformation.height}}0 cm</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <Comments :idPokemon="pokemonInformation.id">
+                        
+                    </Comments>
+                </div>
+
+            </div>
+            
         </div>
+        
     </Modal>
 </template>
 
 <script>
 import Modal from '../../modal/modal.vue'
+import Comments from '../detailsPoekom/commentsPokemon.vue'
 export default {
-    
+    data(){
+        return{
+            id: "1",
+        }
+    },
     components: {
-        Modal
+        Modal,
+        Comments
     },
     props:{
         pokemonInformation:{
